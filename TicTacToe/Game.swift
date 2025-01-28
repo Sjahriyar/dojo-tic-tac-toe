3a0216c5e06a8269c9e6cd02612de26d8135ee65
Sjahriyar <sjahriyar.soheytizadeh@ns.nl>
2025-01-28 10:57:43 +0100

    set initial player to (🟢)
    Passed test, production code ready for Players.

--- TicTacToe/Game.swift
+++ TicTacToe/Game.swift
@@ -1,11 +1,17 @@
 import SwiftUI
 
+enum Player {
+    case x, o
+}
+
 class Game {
     var board: [[String]]
+    var currentPlayer: Player
 
     init() {
         self.board = [[" ", " ", " "],
                       [" ", " ", " "],
                       [" ", " ", " "]]
+        self.currentPlayer = .x
     }
 }
