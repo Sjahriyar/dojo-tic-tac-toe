23f92bb2d72aee8a9e8482d0a6c7e69106e8d17a
Sjahriyar <sjahriyar.soheytizadeh@ns.nl>
2025-01-28 11:26:19 +0100

    Production code add logic to check for vertical wins (🟢)
    Passed test, production code matching the test case

--- TicTacToe/Game.swift
+++ TicTacToe/Game.swift
@@ -1,12 +1,13 @@
 import SwiftUI
 
-enum Player {
-    case x, o
+enum Player: String {
+    case x = "X", o = "O"
 }
 
 class Game {
     var board: [[String]]
     var currentPlayer: Player
+    var winner: Player?
 
     init() {
         self.board = [[" ", " ", " "],
@@ -18,6 +19,18 @@ class Game {
     func makeMove(row: Int, column: Int) {
         guard board[row][column] == " " else { return }
         board[row][column] = currentPlayer == .x ? "X" : "O"
+        checkForWinner()
         currentPlayer = (currentPlayer == .x) ? .o : .x
+    }
+
+    private func checkForWinner() {
+        // Check vertical columns
+        for col in 0..<3 {
+            if board[0][col] == currentPlayer.rawValue &&
+                board[1][col] == currentPlayer.rawValue &&
+                board[2][col] == currentPlayer.rawValue {
+                winner = currentPlayer
+            }
+        }
     }
 }
