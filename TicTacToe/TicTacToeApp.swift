d3c18d20be185b03cd6e2d36758f2a1d9e6c59f9
Sjahriyar <sjahriyar.soheytizadeh@ns.nl>
2025-01-28 10:21:19 +0100

    Initial commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Nami on 28/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
